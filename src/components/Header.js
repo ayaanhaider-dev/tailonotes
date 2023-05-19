21abaaf14c576b9581d78ef2b2f538e0764f7151
StrixersINC <strixersinc@gmail.com>
2023-05-19 18:19:15 +0500

    Bug Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,19 +30,6 @@ const Header = ({ handleToggleDarkMode }) => {
 				Change Theme
 			</button>
   </nav>
-
-  <div className="lg:hidden" role="dialog" aria-modal="true">
-    <div className="fixed inset-0 z-10"></div>
-    <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-      <div className="flex items-center justify-between">
-        <a href="/" className="-m-1.5 p-1.5">
-          <span className="sr-only">TailoNotes</span>
-          <h1>TailoNotes</h1>
-        </a>
-
-      </div>
-    </div>
-  </div>
 </header>
 		</>
 	);
